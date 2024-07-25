0c6aeab6025317ff5535854a622e7cc9518c4e54
Estalin34 <estalinleon06@hotmail.com>
2024-07-24 20:48:50 -0500

    ACTUALIZACION

--- src/main/java/com/springapp/app/Controlador/InicioControlador.java
+++ src/main/java/com/springapp/app/Controlador/InicioControlador.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class LibroControlador {
-    @GetMapping("/libros")
-    public String Libros(){
-        return "libros.html";
+public class InicioControlador {
+    @GetMapping("/index")
+    public String Index(){
+        return "index.html";
     }
 }
